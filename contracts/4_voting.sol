87b822f9d59f979fcd441388c78b79777ab88dbb
NamaN Bhalani <110275891+Sodium-Man@users.noreply.github.com>
2024-08-02 08:18:28 +0530

    add candidates

--- contracts/4_voting.sol
+++ contracts/4_voting.sol
@@ -17,4 +17,43 @@ contract Election {
     event votedEvent (
         uint indexed _candidateId
     );
+
+ constructor () {
+        addCandidate("Bob");
+        addCandidate("Allice");
+       
+    }
+
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+
+    function end () public {
+        goingon = false;
+    }
+
+
+    function vote (uint _candidateId) public {
+        require(!voters[msg.sender],"Already voted");
+
+
+        require(_candidateId > 0 && _candidateId <= candidatesCount,"Invalid candidate");
+
+
+        require(goingon,"Election ended");
+
+
+        voters[msg.sender] = true;
+
+
+        candidates[_candidateId].voteCount ++;
+
+
+        emit votedEvent(_candidateId);
+    }
+}
+
     
